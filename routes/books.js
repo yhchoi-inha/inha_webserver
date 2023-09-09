ae3deec77b8cba907df9bc4a02739b157b508573
Yohan <yohan0312@inha.edu>
2023-09-09 18:40:30 +0900

    add :: 2주차 / 도서 검색 api추가

--- routes/books.js
+++ routes/books.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* page :: books manage */
+/* page :: 도서검색 */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: '도서관리', name : "홍길동" });
+  res.render('index', { title: '도서검색', pageName: 'books/search.ejs'});
 });
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* page :: Main */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express testing' });
+  res.render('index', { title: '안녕하세요', pageName:'home.ejs' });
 });
 
 module.exports = router;
