7774cb1f6cc501bb5b9ee7adbd5dcd3f9e394639
Yohan <yohan0312@inha.edu>
2023-09-23 17:09:29 +0900

    add :: 4주차 / book detail modal

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// routes 파일 등록
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/posts', postsRouter);
