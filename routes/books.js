265d152c161ac2c02906288b637770ac21b90328
Yohan <yohan0312@inha.edu>
2023-10-14 17:55:02 +0900

    add :: 7주차 / add realtime base with cart

--- routes/books.js
+++ routes/books.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: '도서검색', pageName: 'books/search.ejs'});
 });
 
+/* page :: 장바구니 */
+router.get('/cart', function(req, res, next) {
+  res.render('index', { title: '장바구니', pageName: 'books/cart.ejs'});
+});
+
 module.exports = router;
