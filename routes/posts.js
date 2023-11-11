5346f0034c53c23e8fa2164c859ef2d1d018d89f
Yohan <yohan0312@inha.edu>
2023-11-11 17:55:22 +0900

    add :: 11주차 / board

--- routes/posts.js
+++ routes/posts.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
-/* page :: posting */
 router.get('/', function(req, res, next) {
-  res.render('posts', { title: '게시글' });
+  res.render('index', { title: '게시판', pageName:'posts/list.ejs'});
+});
+
+router.get('/insert', function(req, res, next) {
+  res.render('index', { title: '글쓰기', pageName:'posts/insert.ejs'});
 });
 
 module.exports = router;
