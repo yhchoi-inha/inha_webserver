e79e389fcbeb0c58d10d40eea7aa7d419da5ffff
Yohan.choi <yohanchoi.work@gmail.com>
2023-11-18 17:52:20 +0900

    add :: 12주차 / board(repeat)

--- routes/posts.js
+++ routes/posts.js
@@ -9,4 +9,9 @@ router.get('/insert', function(req, res, next) {
   res.render('index', { title: '글쓰기', pageName:'posts/insert.ejs'});
 });
 
+router.get('/read', function(req, res, next) {
+  const id = req.query.id;
+  res.render('index', { title: '게시글 정보', pageName:'posts/read.ejs', id});
+});
+
 module.exports = router;
